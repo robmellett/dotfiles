5a3d308d9a8117241c78703d20458d18356363cd
Rob <dev@robmellett.com>
2021-01-25 23:34:35 +1000

    Add SpaceVim config

--- setup.sh
+++ setup.sh
@@ -6,3 +6,4 @@ ln -s /var/www/dotfiles/conky ~/.config/conky
 ln -s /var/www/dotfiles/dunst ~/.config/dunst
 ln -s /var/www/dotfiles/i3 ~/.config/i3
 ln -s /var/www/dotfiles/rofi ~/.config/rofi
+ln -s /var/www/dotfiles/SpaceVim/init.toml ~/.SpaceVim.d/init.toml
