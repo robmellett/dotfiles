cd0b9b2243a568219a7cdcd2f4891e1b5e8a8ef2
Rob <dev@robmellett.com>
2021-01-27 21:10:35 +1000

    Initial load

--- setup.sh
+++ setup.sh
@@ -5,5 +5,6 @@ ln -s /var/www/dotfiles/alacritty ~/.config/alacritty
 ln -s /var/www/dotfiles/conky ~/.config/conky
 ln -s /var/www/dotfiles/dunst ~/.config/dunst
 ln -s /var/www/dotfiles/i3 ~/.config/i3
+ln -s /var/www/dotfiles/i3status ~/.config/i3status
 ln -s /var/www/dotfiles/rofi ~/.config/rofi
 ln -s /var/www/dotfiles/SpaceVim/init.toml ~/.SpaceVim.d/init.toml
