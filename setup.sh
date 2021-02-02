3b40d07401968a334127b392f09626f32d94032e
Rob <dev@robmellett.com>
2021-02-02 19:52:37 +1000

    Add gtk-3.0

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@
 ln -s /var/www/dotfiles/alacritty ~/.config/alacritty
 ln -s /var/www/dotfiles/conky ~/.config/conky
 ln -s /var/www/dotfiles/dunst ~/.config/dunst
+ln -s /var/www/dotfiles/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
 ln -s /var/www/dotfiles/i3 ~/.config/i3
 ln -s /var/www/dotfiles/i3status ~/.config/i3status
 ln -s /var/www/dotfiles/rofi ~/.config/rofi
