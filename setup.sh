80cb97769bf636def03caddc4c800bc21e7cac78
Rob Mellett <dev@robmellett.com>
2021-10-16 09:14:56 +1000

    Add Sway

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # Setup the configuration files
-ln -s /var/www/dotfiles/alacritty ~/.config/alacritty
-ln -s /var/www/dotfiles/conky ~/.config/conky
-ln -s /var/www/dotfiles/dunst ~/.config/dunst
-ln -s /var/www/dotfiles/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
-ln -s /var/www/dotfiles/i3 ~/.config/i3
-ln -s /var/www/dotfiles/i3status ~/.config/i3status
-ln -s /var/www/dotfiles/rofi ~/.config/rofi
-ln -s /var/www/dotfiles/SpaceVim/init.toml ~/.SpaceVim.d/init.toml
+ln -s ~/www/dotfiles/alacritty ~/.config/alacritty
+ln -s ~/www/dotfiles/conky ~/.config/conky
+ln -s ~/www/dotfiles/dunst ~/.config/dunst
+ln -s ~/www/dotfiles/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
+ln -s ~/www/dotfiles/i3 ~/.config/i3
+ln -s ~/www/dotfiles/i3status ~/.config/i3status
+ln -s ~/www/dotfiles/rofi ~/.config/rofi
+ln -s ~/www/dotfiles/SpaceVim/init.toml ~/.SpaceVim.d/init.toml
+ln -s ~/www/dotfiles/sway ~/.config/sway
+ln -s ~/www/dotfiles/waybar ~/.config/waybar
